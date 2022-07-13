aa457452754d4c6612d0c1c3b95fce6c574163fc
RUNA <taekooklovemy@gmail.com>
2022-07-13 10:17:44 -0300

    First component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,11 @@
-
+import NotificationButton from "./components/notificationButton"
 function App() {
   return (
+    <>
     <h1>teste</h1>
+    <p>ook</p>
+    <NotificationButton></NotificationButton>
+    </>
   )
 }
 
